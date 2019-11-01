25089573a7594104a9c4602f983b8616118fd6ca
Bishop, Devon <n00969927@unf.edu>
2019-11-01 13:21:49 -0400

    Add project files.

---
+++ FindingAMedicalHome_1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindingAMedicalHome_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FindClinics()
+        {
+            ViewBag.Message = "Find Clinics page.";
+
+            return View();
+        }
+
+        public ActionResult ViewDemographics()
+        {
+            ViewBag.Message = "Demographics page.";
+
+            return View();
+        }
+    }
+}
---
+++ FindingAMedicalHome_1/Models/Credentials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Newtonsoft.Json;
+
+namespace FindingAMedicalHome_1.Models
+{
+    public class Credentials
+    {
+        public int ID { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+
+    
+    public class LoginContext : DbContext
+    {
+        public DbSet<Credentials> Credentials { get; set; }
+    }
+}
